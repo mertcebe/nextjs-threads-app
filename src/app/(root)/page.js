b039e34ec89a8910d9c8aaa0e5c2ffef58695951
mertcebe <mr.mertcebe@gmail.com>
2024-02-03 21:33:23 +0300

    created some pages

---
+++ src/app/(root)/page.js
@@ -0,0 +1,10 @@
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="">
+      <UserButton />
+      threads
+    </div>
+  );
+}
